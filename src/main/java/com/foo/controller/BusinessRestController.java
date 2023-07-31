b17801079d3e962f888fee2a3e06594f23324214
leesr94 <sr940718@gmail.com>
2023-08-01 02:22:21 +0900

    Comment : BusinessRestController import 삭제

--- src/main/java/com/foo/controller/BusinessRestController.java
+++ src/main/java/com/foo/controller/BusinessRestController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.foo.domain.RestaurantmenusVO;
 import com.foo.domain.RestaurantsVO;
 
 @RestController
